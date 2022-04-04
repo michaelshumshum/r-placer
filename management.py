897ad0922c5a3a56e2ba8a615c630f30524d3b6c
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2022-04-04 23:49:47 +0800

    better logging info

--- management.py
+++ management.py
@@ -159,15 +159,15 @@ class manager:
         while thread_event.is_set():
             try:
                 time.sleep(random.randint(1, 1 + int(_config.config['worker-count'] / 2)))  # random sleep for less worker conflict
-                if self.queue.empty():
-                    continue
-                coords, color = self.queue.get()
-                Logger.log(f'{current_thread().name} - Setting pixel {coords} on canvas {self.canvas} to color {color}', severity=Logger.Verbose)
                 account = self.choose_account()
                 if not account:
                     Logger.log(f'{current_thread().name} - No accounts available! Waiting 30 seconds.', severity=Logger.Error)
                     time.sleep(30)
                     continue
+                if self.queue.empty():
+                    continue
+                coords, color = self.queue.get()
+                Logger.log(f'{current_thread().name} - Setting pixel {coords} on canvas {self.canvas} to color {color}', severity=Logger.Verbose)
                 r = json.loads(account['class'].set_pixel(coords, color, self.canvas))
                 if 'errors' in r.keys():
                     if (r['errors'][0]['extensions']['nextAvailablePixelTs'] / 1000) - time.time() > 1000:
