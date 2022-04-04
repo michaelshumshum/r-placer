06aaee236b2963c3d9076c7a72ab5b6da9485991
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2022-04-04 22:44:18 +0800

    join random subreddit to improve account validity.

--- account_maker.py
+++ account_maker.py
@@ -1,19 +1,36 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from random import choice
 import _config
 import util
 import time
 import sheets
+
+subs = [
+    'gaming',
+    'csgo',
+    'memes',
+    'tf2',
+    'askreddit',
+    'abruptchaos',
+    'perfectlycutscreams',
+    'askouija',
+    'apple',
+    'linux',
+    'pcmasterrace',
+    'clashroyale',
+    'minecraft',
+    'cursed_comments',
+    'cringetopia',
+    'shitposting'
+]
 
 while True:
     try:
         email = _config.config['account-maker-email']
         username = util.random_letters()
         password = util.random_string()
-
         options = webdriver.ChromeOptions()
         # options.headless = True #disabled for testing
         options.add_extension('extension_1_3_1_0.crx')
@@ -28,15 +45,18 @@ while True:
                 element.send_keys(t)
                 time.sleep(0.1)
 
-        driver.implicitly_wait(5)
+        driver.implicitly_wait(20)
+        driver.get('http://httpbin.org/ip')
         driver.get('https://www.reddit.com/register/')
         email_input = driver.find_element_by_id('regEmail')
         send_keys_better(email_input, email)
         time.sleep(0.5)
         email_input.submit()
-        time.sleep(0.5)
-        send_keys_better(driver.find_element_by_id('regUsername'), username)
-        time.sleep(0.5)
+        time.sleep(1)
+        username_btn = driver.find_element_by_xpath('/html/body/div/main/div[2]/div/div/div[2]/div[2]/div/div/a[1]')
+        username = username_btn.text
+        username_btn.click()
+        time.sleep(1)
         password_input = driver.find_element_by_id('regPassword')
         send_keys_better(password_input, password)
         time.sleep(0.5)
@@ -45,14 +65,21 @@ while True:
         driver.switch_to.parent_frame()
         driver.switch_to_frame(driver.find_element_by_css_selector('iframe[src^="https://www.google.com/recaptcha/api2/bframe?"]'))
         driver.find_element_by_xpath('//*[@id="rc-imageselect"]/div[3]/div[2]/div[1]/div[1]/div[4]').click()
-        time.sleep(7)
+        time.sleep(10)
         driver.switch_to.parent_frame()
-        time.sleep(0.5)
         driver.find_element_by_xpath('/html/body/div[1]/main/div[2]/div/div/div[3]/button').click()
-        driver.find_element_by_xpath('/html/body/div[1]/main/div[2]/div/div/div[3]/button').click()
+        time.sleep(1)
+        try:
+            driver.find_element_by_xpath('/html/body/div[1]/main/div[2]/div/div/div[3]/button').click()
+        except:
+            pass
+        time.sleep(5)
         sheets.add([email, username, password])
+        driver.get(f'https://www.reddit.com/r/{choice(subs)}')
+        driver.find_element_by_xpath('//*[@id="SHORTCUT_FOCUSABLE_DIV"]/div[2]/div/div/div/div[2]/div[1]/div/div[1]/div/div[2]/div[1]/button').click()
         time.sleep(5)
         driver.quit()
+        break
     except KeyboardInterrupt:
         break
     except Exception:
